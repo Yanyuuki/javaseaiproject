3509f9bc3c83dd0b7db574be32a5f9a17890ba52
Yanyuuki <xieyanyu44@gmail.com>
2026-04-02 20:35:42 +0800

    save2

--- day01-helloworld-app/src/com/yanyuuki/variable/variableDemo1.java
+++ day01-helloworld-app/src/com/yanyuuki/variable/variableDemo1.java
@@ -1,6 +1,6 @@
 package com.yanyuuki.variable;
 
-public class variableDemo {
+public class variableDemo1 {
     public static void main(String []args){
         //变量
         int a=10;
@@ -30,6 +30,23 @@ public class variableDemo {
 
         age=200;
         System.out.println(age);
+
+        //Ascii码
+
+        char ch1='A';
+        System.out.println(ch1);
+        System.out.println((int)ch1);
+        System.out.println(ch1+1);
+        System.out.println((char)(ch1+1));
+
+        //直接输入进制
+        System.out.println(0xA);//10,十六进制
+        System.out.println(010);//8,八进制
+        System.out.println(0b1010);//10,二进制
+        System.out.println(0B1010);//10,二进制
+        System.out.println(0xA+1);//11,十六进制
+        System.out.println(010+1);//9,八进制
+        System.out.println(0b1010+1);//11,二进制
 
     }
 }
---
+++ day01-helloworld-app/src/com/yanyuuki/variable/variableDemo2.java
@@ -0,0 +1,37 @@
+package com.yanyuuki.variable;
+
+public class variableDemo2 {
+    public static void main(String[] args) {
+        //整形
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        short s = 10;
+        long l = 10234235235141342L;
+        System.out.println(l);
+        System.out.println(s);
+        System.out.println(c);
+
+        //浮点型,默认是double
+        float f = 10.1f;
+        double d = 10.1;
+        System.out.println(f);
+        System.out.println(d);
+
+        //字符型
+        char ch = 'a';
+        System.out.println(ch);
+
+        //布尔型
+        boolean flag = true;
+        System.out.println(flag);
+
+        //字符串型
+        String str = "hello,world!";
+        System.out.println(str);
+        System.out.println("hello,world!");
+
+
+
+    }
+}
